b26e0ea50483294724cba0f5f813f7f2d231228c
liam7design <lims7design@gmail.com>
2025-07-04 14:47:33 +0900

    로또번호 Ball UI 수정

--- src/components/lotto/Ball.js
+++ src/components/lotto/Ball.js
@@ -12,21 +12,21 @@ const getBallStyle = (num) => {
   } else if (num <= 30) {
     backgroundColor = '#a793b9'; // Viola
   } else if (num <= 40) {
-    backgroundColor = '#a47764'; // Mocha Mousse
+    backgroundColor = '#d19c97'; // Rose Tan
   } else {
-    backgroundColor = '#9a8b4e'; // Willow
+    backgroundColor = '#a47764'; // Mocha Mousse
   }
 
   return {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    width: 36,
-    height: 36,
+    width: 40,
+    height: 40,
     borderRadius: '50%',
     backgroundColor,
     color: 'white',
-    fontSize: '1rem', // 폰트 크기 증가
+    fontSize: '1.125rem', // 폰트 크기 증가
     fontWeight: 'bold',
     // boxShadow: '0 4px 8px rgba(0,0,0,0.2)', // 그림자를 더 은은하게
     boxShadow: '0 2px 5px rgba(0,0,0,0.1), 0 0 0 1px rgba(255,255,255,0.1) inset', // 더 세련된 그림자
